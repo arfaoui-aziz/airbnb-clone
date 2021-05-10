6aaaeb2bfb74ad63053dd83153c4aed801e9c3bf
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-05-10 18:04:37 +0100

    Property Schema init

---
+++ schemas/propertyImage.js
@@ -0,0 +1,14 @@
+export default {
+  name: "propertyImage",
+  title: "Property Image",
+  type: "image",
+  fields: [
+    {
+      name: "caption",
+      title: "Caption",
+      type: "string",
+      options: { isHighlighted: true },
+    },
+  ],
+  options: { hotspot: true },
+};
