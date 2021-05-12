68546efd7bd9db57fb637fd8cc814418c1d068b5
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-05-12 17:23:29 +0100

    fix: og image

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -13,7 +13,10 @@ function MyApp({ Component, pageProps }) {
           content="Airbnb clone using Next.js and Sanity.io"
         />
         <meta property="og:title" content="Airbnb Clone" />
-        <meta property="og:image" content="../images/airbnb-logo.png" />
+        <meta
+          property="og:image"
+          content="https://assets.stickpng.com/images/580b57fcd9996e24bc43c513.png"
+        />
       </Head>
       <NavBar />
       <Component {...pageProps} />{" "}
